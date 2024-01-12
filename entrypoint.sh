9b4285e80fed14860be4330f3368fdc2d75da493
longxuan <admin@remiliascarlet.com>
2024-01-12 20:26:18 +0800

    bump version and update README

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sed -i "s/0\.0\.0\.0/$ADDRESS/g" /app/txlib/$VERSION/config.json
-sed -i "s/8080/$PORT/g" /app/txlib/$VERSION/config.json
+sed -i "s/0\.0\.0\.0/$ADDRESS/g" txlib/$VERSION/config.json
+sed -i "s/8080/$PORT/g" txlib/$VERSION/config.json
 
-echo Start sign service at $ADDRESS:$PORT
+echo Start sign service at $ADDRESS:$PORT, workdir=txlib/$VERSION
 
-/bin/sh ./bin/unidbg-fetch-qsign --basePath=./txlib/$VERSION
+/bin/sh ./bin/unidbg-fetch-qsign --basePath=txlib/$VERSION
